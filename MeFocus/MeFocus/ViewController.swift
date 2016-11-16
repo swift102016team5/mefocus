bedf64f49009eafd50031a69b01acb76a5985c74
Enta'ard <dratannta@gmail.com>
2016-11-16 19:06:32 +0700

    merge tuananh

---
+++ MeFocus/MeFocus/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MeFocus
+//
+//  Created by Enta'ard on 11/15/16.
+//  Copyright © 2016 Group5. All rights reserved.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
